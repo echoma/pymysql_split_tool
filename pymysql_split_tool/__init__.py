3382ad43bcd566f055eee2bae4c9a5a348bd0387
echo <fatmck@163.com>
2018-11-22 17:14:20 +0800

    修复pypi打包错误

--- pymysql_split_tool/__init__.py
+++ pymysql_split_tool/__init__.py
@@ -1,13 +1,15 @@
-import input
-import controller
+name = 'pymysql_split_tool'
 
 __version__ = '0.5.6'
 
 def init(action, task, debug=False):
+    import input
     input.init(action, task, debug)
 
 def init_by_cmd_line_args():
+    import input
     input.init_by_cmd_line_args()
 
 def do_work():
+    import controller
     controller.do_work()
--- pymysql_split_tool/__main__.py
+++ pymysql_split_tool/__main__.py
@@ -1,4 +1,4 @@
-from __init__ import *
+from __init__ import init_by_cmd_line_args, do_work
 
 if __name__ == '__main__':
     init_by_cmd_line_args()
